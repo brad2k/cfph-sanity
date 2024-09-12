6abaa3fe666d9c92e63fc70019574f79e2900eb4
Brad Azevedo <brad2k@gmail.com>
2024-09-12 15:08:35 -0700

    Updates to packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.squarespace-cdn.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.squarespace-cdn.com",
+        port: "",
+      },
+    ],
   },
 };
 
